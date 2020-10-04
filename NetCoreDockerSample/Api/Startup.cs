4d23e4c1758883cf7fbd835982b1a81f782d08f2
Rogerio Schmitt <rogerio.schmitt@hbsis.com.br>
2020-10-04 17:34:56 -0300

    Add docker environment

--- NetCoreDockerSample/Api/Startup.cs
+++ NetCoreDockerSample/Api/Startup.cs
@@ -21,7 +21,7 @@ namespace Api
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -35,6 +35,16 @@ namespace Api
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            
+            Console.WriteLine($"Running on environment: {env.EnvironmentName}");
+            
             var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
             var context = serviceScope.ServiceProvider.GetRequiredService<Context>();
             context.Database.Migrate();
