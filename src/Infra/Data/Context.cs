8469a729b98ef4a336b6f7d144e6e9d5a6aa6fb2
Rogerio Schmitt <rogerio.schmitt@me.com>
2021-04-15 17:54:18 -0300

    Remove unused code

--- src/Infra/Data/Context.cs
+++ src/Infra/Data/Context.cs
@@ -5,17 +5,7 @@ namespace Infra.Data
 {
     public class Context : DbContext
     {
-        public Context() { }
-        
         public Context(DbContextOptions<Context> options) : base(options) { }
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("DefaultConnection");
-            }
-        }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
